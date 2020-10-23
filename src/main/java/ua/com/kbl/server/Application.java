d781dfbaf76d6d31fddfd5d6f3bdf50549e7a045
Yevhenii Skliarov <yevhenii.skliarov@logika.com.ua>
2020-10-23 13:44:30 +0300

    change port to 8083

--- src/main/java/ua/com/kbl/server/Application.java
+++ src/main/java/ua/com/kbl/server/Application.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8083"));
+        app.run(args);
     }
 }
